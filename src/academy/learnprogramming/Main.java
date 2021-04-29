a16ac69ee8417af2f4130f54d5407a81a0abcd80
Richard <richard.yang.tong@gmail.com>
2021-04-29 17:57:47 -0400

    Added outputs as comments.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -27,23 +27,28 @@ public class Main {
         System.out.println("previousResult = " + previousResult);
 
         // result = result + 1;
-        result++; // 1 + 1 + 2
+        result++; // 1 + 1 = 2
         System.out.println("1 + 1 = " + result);
         System.out.println("previousResult = " + previousResult);
 
         // result = result - 1;
-        result--;
+        result--; // 2 - 1 = 1
         System.out.println("2 - 1 = " + result);
         System.out.println("previousResult = " + previousResult);
 
         // result = result + 2;
-        result += 2;
+        result += 2; // 1 + 2 = 3
         System.out.println("1 + 2 = " + result);
         System.out.println("previousResult = " + previousResult);
 
         // result = result * 10;
-        result *= 10;
+        result *= 10; // 3 * 10 = 30
         System.out.println("3 * 10 = " + result);
+        System.out.println("previousResult = " + previousResult);
+
+        // result = result / 3;
+        result /= 3; // 30 / 3 = 10
+        System.out.println("30 / 3 = " + result);
         System.out.println("previousResult = " + previousResult);
     }
 }
