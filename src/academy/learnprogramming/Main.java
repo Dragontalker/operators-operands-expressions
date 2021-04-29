11f4dcb908e38c85fb6efbf49dcc13d581ab56b1
Richard <richard.yang.tong@gmail.com>
2021-04-29 17:54:58 -0400

    Added a abbitrary incremental.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -35,5 +35,10 @@ public class Main {
         result--;
         System.out.println("2 - 1 = " + result);
         System.out.println("previousResult = " + previousResult);
+
+        // result = result + 2;
+        result += 2;
+        System.out.println("1 + 2 = " + result);
+        System.out.println("previousResult = " + previousResult);
     }
 }
