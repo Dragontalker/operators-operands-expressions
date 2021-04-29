5c3a341df6a54d15d95bac6dd627890808c0a482
Richard <richard.yang.tong@gmail.com>
2021-04-29 09:39:31 -0400

    Added an example for assignment expression.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -6,5 +6,13 @@ public class Main {
 
         int result = 1 + 2; // 1 + 2 = 3
         System.out.println("1 + 2 = " + result);
+
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+
+        result = result - 1; // 3 - 1 = 2
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previousResult = " + previousResult);
+
     }
 }
